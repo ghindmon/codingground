78902553967ffe7ba633d03515474bad9dca1d34
ghindmon <ghindmon@users.noreply.github.com>
2015-06-04 10:06:10 -0400

    Created by Coding Ground Client

---
+++ AL Problem 1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MOD %   // modulo operator
+#define OR  ||  // logical or operator
+
+//------------------------------------------------------------------------------
+// Function:    int SumMultiples(int MaxNumber)
+// Description: Sums all natural numbers less than
+// Parameters:
+// Returns:
+//------------------------------------------------------------------------------
+
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
